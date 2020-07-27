c0d254e6c74e03112f77d93b95ef50a2eb5b4221
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-07-27 12:53:25 -0500

    Add version, MANIFEST.in and setup.cfg (Closes #9)

--- element/__init__.py
+++ element/__init__.py
@@ -3,3 +3,14 @@ from .element import element_from_name
 from .element import element_from_atomic_number
 from .element import element_from_mass
 from .element import Elements
+
+__version__ = '0.0.0'
+
+__all__ = [
+    '__version__',
+    'element_from_symbol',
+    'element_from_name',
+    'element_from_atomic_number',
+    'element_from_mass',
+    'Elements'
+]
