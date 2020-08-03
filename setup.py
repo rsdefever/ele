1444b179f42b64c92ae30fd76ec4d0ecd96b5eef
Ryan S. DeFever <r.s.defever@gmail.com>
2020-08-03 13:31:00 -0400

    Remove old versioning info

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from setuptools import setup, find_packages
 
-#####################################
-VERSION = "0.0.0"
-ISRELEASED = False
-if ISRELEASED:
-    __version__ = VERSION
-else:
-    __version__ = VERSION + ".dev0"
-#####################################
-
 requirements = [
 ]
 
